fd5970f716689271e8f81797c59ab2ee59cdcddc
Farhat Raza <razafarhat8@gmail.com>
2021-08-13 15:05:50 -0400

    Validation Done

--- js/main.js
+++ js/main.js
@@ -1,11 +1,66 @@
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
+function validateForm() {
+    let x = document.forms["contact-form"]["firstName"].value;
+    if (x == "") {
+        document.getElementById("firstName_vText").innerHTML = "First Name is required";
+        $("#firstName_vText").removeAttr('hidden');
+        return false;
     }
+    x = document.forms["contact-form"]["lastName"].value;
+    if (x == "") {
+        document.getElementById("lastName_vText").innerHTML = "Last Name is required";
+        $("#lastName_vText").removeAttr('hidden');
+        return false;
+    }
+    x = document.forms["contact-form"]["address"].value;
+    if (x == "") {
+        document.getElementById("address_vText").innerHTML = "Address is required";
+        $("#address_vText").removeAttr('hidden');
+        return false;
+    }
+    x = document.forms["contact-form"]["city"].value;
+    if (x == "") {
+        document.getElementById("city_vText").innerHTML = "City is required";
+        $("#city_vText").removeAttr('hidden');
+        return false;
+    }
+    x = document.forms["contact-form"]["province"].value;
+    if (x == "") {
+        document.getElementById("province_vText").innerHTML = "Province is required";
+        $("#province_vText").removeAttr('hidden');
+        return false;
+    }
+    x = document.forms["contact-form"]["postalCode"].value;
+    if (x == "") {
+        document.getElementById("postalCode_vText").innerHTML = "PostalCode pattern is required NAN ANA ";
+        $("#postalCode_vText").removeAttr('hidden');
+        return false;
+    }
+    x = document.forms["contact-form"]["phoneNumber"].value;
+    if (x == "") {
+        document.getElementById("phoneNumber_vText").innerHTML = "PhoneNumber is required";
+        $("#phoneNumber_vText").removeAttr('hidden');
+        return false;
+    } else if (!x.value.match(/^\d{10}$/)) {
+        document.getElementById("phoneNumber_vText").innerHTML = "PhoneNumber pattern is (111)111-1111";
+        return false;
+    }
+    x = document.forms["contact-form"]["emailAddress"].value;
+    if (x == "") {
+        document.getElementById("emailAddress_vText").innerHTML = "Email Address is required";
+        $("#emailAddress_vText").removeAttr('hidden');
+        return false;
+    }
+    x = document.forms["contact-form"]["comments"].value;
+    if (x == "") {
+        document.getElementById("comments_vText").innerHTML = "Note Field is required";
+        $("#comments_vText").removeAttr('hidden');
+        return false;
+    }
+    saveContact();
+    return true;
 }
+
+// Functions for Save Contact
 
 function saveContact() {
     let formData = {
